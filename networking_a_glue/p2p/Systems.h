ff647b0c6eff323d6668bdc8964ea5be93a5a31f
radhamadhabdalai <rmdi115@gmail.com>
2024-08-31 19:44:42 +0530

    p2p

--- networking_a_glue/p2p/Systems.h
+++ networking_a_glue/p2p/Systems.h
@@ -1,18 +1,12 @@
-//
-// ==================================
-// libeom
-//
-// an open source c library.
-// ==================================
-//
-// Systems.h
-//
-// Eric Meehan
-// 3/1/21
-//
-//
 
-
+/* Copyright (C) 2022 Radhamadhab Dalai - All Rights Reserved
+ * You may use, distribute and modify this code under the
+ * terms of the license, which unfortunately won't be
+ * written for another century.
+ *
+ * You should have received a copy of the license with
+ * this file. If not, please write to: radhamadhabdalai@soa.ac.in.
+ */
 
 #ifndef System_h
 #define System_h
@@ -21,4 +15,4 @@
 
 #include "Systems/Files.h"
 
-#endif /* System_h */
+#endif 
